92e0182c533431e694e15b7d32dc6b696c68ec38
ezebonasegla <ebonasegla@gmail.com>
2022-11-07 01:36:23 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tres en línea - TaTeTi
+
+<img src="https://i.ibb.co/BybrwRh/Screen-Shot-2021-02-07-at-23-52-27.png" alt="tateti" width="400"/>
---
+++ src/components/VozRecognition/VozRecognition.js
@@ -0,0 +1,27 @@
+import './VozRecognition.css';
+import { useState } from 'react';
+
+const VozRecognition = ({onVoice}) => {
+    const [voice, setVoice] = useState(null);
+    
+    const handleVoice = () => {
+        const recognition = new window.webkitSpeechRecognition();
+        recognition.lang = 'es-ES';
+        recognition.start();
+        recognition.onresult = (event) => {
+        const speechToText = event.results[0][0].transcript;
+        setVoice(speechToText);
+        onVoice(speechToText);
+        }
+    }
+
+    return (
+        <div className="VozRecognition">
+        <button onClick={handleVoice}>Escuchar</button>
+        <h2 className="voiceText">Usted ha dicho: {voice}</h2>
+        {voice === 'uno' || voice === '1' || voice === 'dos' || voice === '2' || voice === 'tres' || voice === '3' || voice === 'cuatro' || voice === '4' || voice === 'cinco' || voice === '5' || voice === 'seis' || voice === '6' || voice === 'siete' || voice === '7' || voice === 'ocho' || voice === '8' || voice === 'nueve' || voice === '9' || voice === 'reiniciar' || voice === null ? <></> : <h3 className="incorrect">Incorrecto</h3>}
+        </div>
+    );
+    }
+
+export default VozRecognition;
