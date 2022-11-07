2f1ad34a62f1af136b8960fe70140e431ca460e4
Ezequiel Bonasegla <ebonasegla@gmail.com>
2022-11-07 12:42:23 -0300

    Update VozRecognition.js

--- src/components/VozRecognition/VozRecognition.js
+++ src/components/VozRecognition/VozRecognition.js
@@ -20,7 +20,7 @@ const VozRecognition = ({onVoice}) => {
         <div className="VozRecognition">
         <button onClick={handleVoice}>Escuchar</button>
         <h2 className="voiceText">Usted ha dicho: {voice}</h2>
-        {voice === 'uno' || voice === '1' || voice === 'dos' || voice === '2' || voice === 'tres' || voice === '3' || voice === 'cuatro' || voice === '4' || voice === 'cinco' || voice === '5' || voice === 'seis' || voice === '6' || voice === 'siete' || voice === '7' || voice === 'ocho' || voice === '8' || voice === 'nueve' || voice === '9' || voice === 'reiniciar' || voice === null ? <></> : <h3 className="incorrect">Incorrecto</h3>}
+        {voice === 'uno' || voice === 'Uno' || voice === '1' || voice === 'dos' || voice === 'Dos' || voice === '2' || voice === 'tres' || voice === 'Tres' || voice === '3' || voice === 'cuatro' || voice === 'Cuatro' || voice === '4' || voice === 'cinco' || voice === 'Cinco' || voice === '5' || voice === 'seis' || voice === 'Seis' || voice === '6' || voice === 'siete' || voice === 'Siete' || voice === '7' || voice === 'ocho' || voice === 'Ocho' || voice === '8' || voice === 'nueve' || voice === 'Nueve' || voice === '9' || voice === 'reiniciar' || voice === 'Reiniciar' || voice === null ? <></> : <h3 className="incorrect">Incorrecto</h3>}
         </div>
     );
     }
