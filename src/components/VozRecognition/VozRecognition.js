0652f9494d8cd0a74826c565720fd4a34bcc75d8
AlejoVeronese <alejoveronese2022@gmail.com>
2022-11-08 17:02:24 -0300

    Reconocimiento de voz automatico

--- src/components/VozRecognition/VozRecognition.js
+++ src/components/VozRecognition/VozRecognition.js
@@ -1,26 +1,57 @@
 import './VozRecognition.css';
 import { useState } from 'react';
 
+const toString = (entry)=>{
+    let res=""
+    if (typeof entry !== "string") {
+        res =entry.toString();
+    }
+    else{
+        res=entry.toLowerCase();
+    }
+    return res
+}
+const possibilities = ['1','2','3','4','5','6','7','8','9',
+                    'uno','dos','tres','cuatro','cinco','seis','siete','ocho','nueve',
+                    'reiniciar']
+
 const VozRecognition = ({onVoice}) => {
     const [voice, setVoice] = useState(null);
-    
-    const handleVoice = () => {
-        const recognition = new window.webkitSpeechRecognition() || new window.SpeechRecognition();
-        recognition.lang = 'es-ES';
-        recognition.continuous = false
-        recognition.start();
-        recognition.onresult = (event) => {
-        const speechToText = event.results[0][0].transcript;
-        setVoice(speechToText);
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new window.SpeechRecognition();
+    recognition.lang = 'es-ES';
+    recognition.interimResults = false;
+    recognition.continuous = false;
+    recognition.addEventListener('result', (e)=>{
+        console.log(e.results[0][0].transcript)
+        const speechToText = e.results[0][0].transcript;
+        let res=toString(speechToText)
+        setVoice(res);
         onVoice(speechToText);
-        }
-    }
+
+    })
+    // recognition.addEventListener('end',()=>{
+    // recognition.start();
+    // })
+    recognition.start();
+
+    // const handleVoice = () => {
+    //     const recognition = new window.webkitSpeechRecognition() || new window.SpeechRecognition();
+    //     recognition.lang = 'es-ES';
+    //     recognition.continuous = false
+    //     recognition.start();
+    //     recognition.onresult = (event) => {
+    //     const speechToText = event.results[0][0].transcript;
+    //     let res=toString(speechToText)
+    //     setVoice(res);
+    //     onVoice(speechToText);
+    //     }
+    // }
 
     return (
         <div className="VozRecognition">
-        <button onClick={handleVoice}>Escuchar</button>
-        <h2 className="voiceText">Usted ha dicho: {voice}</h2>
-        {voice === 'uno' || voice === 'Uno' || voice === '1' || voice === 'dos' || voice === 'Dos' || voice === '2' || voice === 'tres' || voice === 'Tres' || voice === '3' || voice === 'cuatro' || voice === 'Cuatro' || voice === '4' || voice === 'cinco' || voice === 'Cinco' || voice === '5' || voice === 'seis' || voice === 'Seis' || voice === '6' || voice === 'siete' || voice === 'Siete' || voice === '7' || voice === 'ocho' || voice === 'Ocho' || voice === '8' || voice === 'nueve' || voice === 'Nueve' || voice === '9' || voice === 'reiniciar' || voice === 'Reiniciar' || voice === null ? <></> : <h3 className="incorrect">Incorrecto</h3>}
+        {/* <button onClick={handleVoice}>Escuchar</button> */}
+        {possibilities.includes(voice) || voice === null ? <h2 className="voiceText">Usted ha dicho: {voice}</h2> : <h3 className="incorrect">Incorrecto</h3>}
         </div>
     );
     }
